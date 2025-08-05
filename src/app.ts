538cf46d969b6c2039fc49635dbd2f11025c505a
Atikur Rahaman <atikurrahaman0305@gmail.com>
2025-08-05 21:23:16 +0600

    Implement user routes and controller for admin creation; set up user service with createAdmin method

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
 import cors from "cors";
 import express, { Application } from "express";
+import { userRoutes } from "./app/modules/User/user.routes";
 
 const app: Application = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); // Include this line to parse URL-encoded bodies
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the Healthcare Server API");
-});
+app.use("/api/v1/user", userRoutes);
 
 export default app;
