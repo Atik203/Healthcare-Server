62e9724fd9bba2c30aac7b60ab726060faf2f161
Atikur Rahaman <atikurrahaman0305@gmail.com>
2025-08-05 19:47:09 +0600

    Refactor server and app structure: move middleware and routes to app.ts for better organization

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import cors from "cors";
+import express, { Application } from "express";
+
+const app: Application = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Healthcare Server API");
+});
+
+export default app;
