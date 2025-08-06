5083839225049e36509cdbff20d4780d12b3e8e3
Atikur Rahaman <atikurrahaman0305@gmail.com>
2025-08-07 01:33:18 +0600

    Refactor getAllAdmin method to destructure searchTerm from params for improved readability

--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -5,13 +5,23 @@ const prisma = new PrismaClient();
 const getAllAdmin = async (params: any) => {
   const searchFields = ["name", "email"];
   const conditions: any[] = [];
-
+  const { searchTerm, ...filterData } = params;
   if (params.searchTerm) {
     conditions.push({
       OR: searchFields.map((field) => ({
         [field]: {
-          contains: params.searchTerm,
+          contains: searchTerm,
           mode: "insensitive",
+        },
+      })),
+    });
+  }
+
+  if (Object.keys(filterData).length > 0) {
+    conditions.push({
+      AND: Object.entries(filterData).map(([key, value]) => ({
+        [key]: {
+          equals: value,
         },
       })),
     });
