0975b0ef36da80be22a6c8d861a1ffbc71eb2219
Atikur Rahaman <atikurrahaman0305@gmail.com>
2025-08-07 01:45:38 +0600

    Auto PR: Merge branch 'dev' of https://github.com/Atik203/Healthcare-Server into dev (#47)
    * Refactor getAllAdmin method to use OR condition for searching by name or email
    
    * Refactor getAllAdmin method to build conditions dynamically for search functionality
    
    * Refactor getAllAdmin method to simplify search conditions using dynamic mapping for name and email
    
    * Refactor getAllAdmin method to destructure searchTerm from params for improved readability
    
    * Refactor getAllAdmin method to use destructured searchTerm for condition checks
    
    * Refactor getAllAdmin method to filter query parameters using a pick function

--- src/app/modules/admin/admin.controller.ts
+++ src/app/modules/admin/admin.controller.ts
@@ -1,9 +1,22 @@
 import { Request, Response } from "express";
 import { adminService } from "./admin.service";
 
+const pick = (obj: any, keys: string[]) => {
+  return Object.fromEntries(
+    Object.entries(obj).filter(([key]) => keys.includes(key))
+  );
+};
+
 const getAllAdmin = async (req: Request, res: Response) => {
   try {
-    const result = await adminService.getAllAdmin(req.query);
+    const filteredQuery = pick(req.query, [
+      "searchTerm",
+      "email",
+      "contactNumber",
+      "name",
+    ]);
+
+    const result = await adminService.getAllAdmin(filteredQuery);
     return res.status(200).json({
       status: "success",
       message: "Admins retrieved successfully",
