086182ed4d8da19773e6431dffd140f784d5949a
Atikur Rahaman <atikurrahaman0305@gmail.com>
2025-08-07 01:09:14 +0600

    Refactor getAllAdmin method to use OR condition for searching by name or email (#34)

--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -5,10 +5,20 @@ const prisma = new PrismaClient();
 const getAllAdmin = async (params: any) => {
   const result = await prisma.admin.findMany({
     where: {
-      name: {
-        contains: params.searchTerm,
-        mode: "insensitive",
-      },
+      OR: [
+        {
+          name: {
+            contains: params.searchTerm,
+            mode: "insensitive",
+          },
+        },
+        {
+          email: {
+            contains: params.searchTerm,
+            mode: "insensitive",
+          },
+        },
+      ],
     },
   });
   return result;
