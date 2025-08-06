2722b4bb925666301c478549e105855b7327c89f
Atikur Rahaman <atikurrahaman0305@gmail.com>
2025-08-07 01:06:17 +0600

    Remove unused route definitions for user in routes index file

--- src/app/modules/routes/index.ts
+++ src/app/modules/routes/index.ts
@@ -1,22 +1,6 @@
 import express from "express";
-import { userRoutes } from "../User/user.routes";
-
-const router = express.Router();
-
-const routes = [
-  {
-    path: "/user",
-    route: userRoutes,
-  },
-];
-
-routes.forEach((route) => {
-  router.use(route.path, route.route);
-});
-
-export default router;
-
 import { adminRoutes } from "../admin/admin.routes";
+import { userRoutes } from "../user/user.routes";
 
 const router = express.Router();
 
