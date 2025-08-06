444f6401ff48a59cd4dfd36bdb528200a8d3a170
Atikur Rahaman <atikurrahaman0305@gmail.com>
2025-08-07 00:18:15 +0600

    Refactor user routes and controller for admin creation; implement user service to handle admin creation logic

--- src/app/modules/User/user.service.ts
+++ src/app/modules/User/user.service.ts
@@ -1,5 +1,33 @@
-const createAdmin = async () => {
-  console.log("Creating admin user...");
+import { PrismaClient, UserRole } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const createAdmin = async (data: any) => {
+  const userData = {
+    email: data.admin.email,
+    password: data.password,
+    role: UserRole.ADMIN,
+  };
+
+  const adminData = {
+    name: data.admin.name,
+    email: data.admin.email,
+    contactNumber: data.admin.contactNumber,
+  };
+
+  const result = await prisma.$transaction(async (tx: PrismaClient) => {
+    const user = await tx.user.create({
+      data: userData,
+    });
+
+    const admin = await tx.admin.create({
+      data: adminData,
+    });
+
+    return { user, admin };
+  });
+
+  return result;
 };
 
 export const userService = {
