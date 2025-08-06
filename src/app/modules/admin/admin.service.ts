234196bc757ef5c5b6a6030d11c17f8fb493766c
Atikur Rahaman <atikurrahaman0305@gmail.com>
2025-08-07 01:26:12 +0600

    Auto PR: Merge branch 'main' into dev (#37)
    * Refactor getAllAdmin method to use OR condition for searching by name or email
    
    * Refactor getAllAdmin method to build conditions dynamically for search functionality
    
    * Refactor getAllAdmin method to simplify search conditions using dynamic mapping for name and email

--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -3,26 +3,19 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const getAllAdmin = async (params: any) => {
-  const result = await prisma.admin.findMany({
-    where: {
-      OR: [
-        {
-          name: {
-            contains: params.searchTerm,
-            mode: "insensitive",
-          },
+  const searchFields = ["name", "email"];
+  const conditions: any[] = [];
+
+  if (params.searchTerm) {
+    conditions.push({
+      OR: searchFields.map((field) => ({
+        [field]: {
+          contains: params.searchTerm,
+          mode: "insensitive",
         },
-        {
-          email: {
-            contains: params.searchTerm,
-            mode: "insensitive",
-          },
-        },
-      ],
-    },
-  });
-  return result;
-};
+      })),
+    });
+  }
 
 export const adminService = {
   getAllAdmin,
